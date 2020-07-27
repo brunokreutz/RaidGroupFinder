7f38a7d3005e2fcd7f1d9cf7dacaaeb46f0d06cd
Bruno Kreutz <kreutz.dev@gmail.com>
2020-07-27 20:41:48 -0300

    Add NavBar; Add raid chat page; Add deploy configs

--- RaidGroupFinder/Hubs/ChatHub.cs
+++ RaidGroupFinder/Hubs/ChatHub.cs
@@ -9,9 +9,34 @@ namespace BlazorSignalRApp.Server.Hubs
 {
     public class ChatHub : Hub
     {
-        public async Task SendMessage(string message)
+        //public async Task SendMessage(string message)
+        //{
+        //    await Clients.All.SendAsync("ReceiveMessage", message);
+        //}
+
+        public async Task SendMessage(string user, string message, string room, bool join)
         {
-            await Clients.All.SendAsync("ReceiveMessage", message);
+            //if (join)
+            //{
+                await JoinRoom(room).ConfigureAwait(false);
+            //    await Clients.Group(room).SendAsync("ReceiveMessage", user, " joined to " + room).ConfigureAwait(true);
+
+            //}
+            //else
+            //{
+                await Clients.Group(room).SendAsync("ReceiveMessage", user, message).ConfigureAwait(true);
+
+            //}
+        }
+
+        public Task JoinRoom(string roomName)
+        {
+            return Groups.AddToGroupAsync(Context.ConnectionId, roomName);
+        }
+
+        public Task LeaveRoom(string roomName)
+        {
+            return Groups.RemoveFromGroupAsync(Context.ConnectionId, roomName);
         }
     }
 }
